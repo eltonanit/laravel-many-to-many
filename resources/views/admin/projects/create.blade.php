daaf603f53408cf2b1171817c4acd983616cccfc
eltonanit <elton.ani@gmail.com>
2024-10-16 18:23:25 +0200

    [FIX] fixed create- checkbox tecnologies

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -35,7 +35,9 @@
                             <div>
                             @foreach ($technologies as $technology )
                                   <div class="form-check-inline">
-                                    <input type="checkbox"  name="technologies[]" id="" class="form-check-inline"  value="{{$technology->id}}">
+                                    <input type="checkbox" name="technologies[]" id="" 
+                                    class="form-check-inline" value="{{$technology->id}}" 
+                                    {{ is_array(old('technologies')) && in_array($technology->id, old('technologies')) ? 'checked' : '' }}>
                                     <label class="form-check-label"> {{$technology->name}}</label>
                                 
                                 @endforeach
